20cb4effc99d03a4020a26dbf2cedbc245747fc0
yushare <582718643@qq.com>
2019-06-24 14:41:50 +0800

    update

--- send.js
+++ send.js
@@ -2,7 +2,7 @@ const nodemailer  = require("nodemailer");
 const config = require("./config");
 
 // 参数：发件人，收件人，主题，正文（支持html格式）
-function handleSend (aliasName, tos, subject, body) {
+function handleSend (tos, subject, body) {
   const smtpTransport = nodemailer.createTransport({
     host: 'smtp.163.com',
     secureConnection: true, // use SSL
@@ -15,8 +15,7 @@ function handleSend (aliasName, tos, subject, body) {
   })
 
   smtpTransport.sendMail({
-    //from    : '标题别名 <foobar@latelee.org>',
-    from    : aliasName + ' ' + '<' + from + '>',
+    from    : `自如租房 < ${config.user} >`,
     //'li@latelee.org, latelee@163.com',//收件人邮箱，多个邮箱地址间用英文逗号隔开
     to      : tos,
     subject : subject,//邮件主题
@@ -31,7 +30,6 @@ function handleSend (aliasName, tos, subject, body) {
 
 function send() {
   handleSend(
-    '自如房子可以预约了！',
     "582718643@qq.com",
     '赶紧去APP预约吧！',
     'Goooooooooooooooooo!'
